20836920e8d90fa03961b814d701df870d89bfac
Juan <jdutoit@retrorabbit.co.za>
2024-10-08 08:18:43 +0200

    added better identifier

--- apps/web/app/api/data/route.tsx
+++ apps/web/app/api/data/route.tsx
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export async function GET(req: Request) {
   try {
   const { rows } = await sql`SELECT * from CARTS`;
-  return NextResponse.json({ projects: rows }, { status: 200 });
+  return NextResponse.json({ carts: rows }, { status: 200 });
   }
   catch (error) {
     console.error("Database query failed:", (error as Error).message);
